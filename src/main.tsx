12bca4e5b8eb73604c88ec55a16bd0e8a090cc9f
Yasemin KOÇ <yaseminkocc37@gmail.com>
2026-08-22 22:11:16 +0300

    feat: switch from BrowserRouter to HashRouter for improved routing compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+// HashRouter: rotalar hash içinde tutulur (snaphai.com/app/#/connect).
+// Böylece herhangi bir statik sunucuda derin bağlantılar (yenileme/direkt açma)
+// sunucu tarafı rewrite'a ihtiyaç duymadan çalışır. base yine /app/ (varlıklar için).
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/app">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
